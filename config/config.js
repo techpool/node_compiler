0bdadf47569052e70f09cc15f7e6830ab9d16a08
Suryadeep Pal <suryadeep10@gmail.com>
2016-07-03 02:38:20 +0530

    passport.js configured for login

--- config/config.js
+++ config/config.js
@@ -2,6 +2,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var passport = require('passport')
+var LocalStrategy = require('passport-local').Strategy
+var session = require('express-session')
+var flash = require('connect-flash');
 
 module.exports = function (app, express) {
 	
@@ -11,6 +15,53 @@ module.exports = function (app, express) {
 	app.use(bodyParser.json());
 	app.use(bodyParser.urlencoded({ extended: false }));
 	app.use(cookieParser());
+	app.use(flash());
+
+	app.use(session({
+		secret: 'thisisa complete random key, pamelapamela',
+		resave: true,
+    	saveUninitialized: true
+	}));
+	app.use(passport.initialize());
+	app.use(passport.session());
+
+	passport.serializeUser(function(user, done) {
+		done(null, user.id);
+	});
+
+	passport.deserializeUser(function(id, done) {
+		Users.findById(id, function(err, user) {
+			done(err, user);
+		});
+	});
+
+	passport.use(new LocalStrategy(
+		{
+			usernameField: 'email',
+			passwordField: 'password'
+		},
+		function(email, password, done) {
+			Users.findOne({ email: email }, function (err, user) {
+				if (err) {
+					return done(err);
+				}
+				if (!user) {
+					return done(null, false, { message: 'Incorrect username.' });
+				}
+				user.validatePassword(password, function(err, isMatch) {
+					if (err) {
+						return done(err);
+					} else {
+						if (isMatch) {
+							return done(null, user);
+						} else {
+							return done(null, false, { message: 'Incorrect password.' });
+						}
+					}
+				});
+			});
+		}
+	));
 
 	// DB Connector
 	
--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport')
+
+redirects = { 
+	successRedirect: '/code',
+	failureRedirect: '/login',
+	failureFlash : true
+}
 
 // GET the login page
 router.get('/', function(req, res, next) {
+	console.log(req.flash());
 	res.render('login');
+});
+
+router.post('/',
+	passport.authenticate('local', redirects),
+	function(req, res, next) {
+		res.send('Congo you are in');
 });
 
 module.exports = router;
